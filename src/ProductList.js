d1e18280a126506ba3d5fd23d0b6ff99e1bba39d
Justin Dennison <justin1dennison@gmail.com>
2018-03-08 19:58:08 -0500

    rendered individual products in a product list

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
+const Product = props => {
+  return (
+    <li>
+      <img src={props.imgSrc} alt={props.name} />
+      <p>{props.name}</p>
+      <p>${props.price}</p>
+      <button onClick={() => console.log("+")}>+</button>
+      <p>{props.count}</p>
+      <button onClick={() => console.log("-")}>-</button>
+    </li>
+  );
+};
 const ProductList = props => {
-    const ProductItems = props.products
-                              .map(product => product.name)
-                              .map(name => <li>{name}</li>);
-    return (
-        <ul>
-            {ProductItems}
-        </ul>
-    );
-}
+  const ProductItems = props.products.map((product, index) => (
+    <Product {...product} key={index} />
+  ));
+  return <ul>{ProductItems}</ul>;
+};
 
 export default ProductList;
