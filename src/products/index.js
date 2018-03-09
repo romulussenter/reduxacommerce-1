861665db587b02d547e4ff610a034e701c9af3e2
Justin Dennison <justin1dennison@gmail.com>
2018-03-08 19:27:33 -0500

    created mock initial state for products

--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,25 @@
 
-
-const reducer = (state={}, action) => {
+const initialState = [
+    {
+        imgSrc: "https://via.placeholder.com/100x100",
+        name: "Javelin (caution)",
+        price: 500.00,
+        count: 0
+    },
+    {
+        imgSrc: "https://via.placeholder.com/100x100",
+        name: "Snail",
+        price: 3.99,
+        count: 0
+    },
+    {
+        imgSrc: "https://via.placeholder.com/100x100",
+        name: "Rug",
+        price: 93.47,
+        count: 0
+    }
+];
+const reducer = (state=initialState, action) => {
     console.log('product stuffs');
     return state;
 }
