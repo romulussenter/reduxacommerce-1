e0065f7f726c8da6b78d824c2e79fa7710e98a0a
Justin Dennison <justin1dennison@gmail.com>
2018-03-09 20:40:21 -0500

    restructured the data to an object and applied temporary fix to product list

--- src/ProductList.js
+++ src/ProductList.js
@@ -2,7 +2,8 @@ import React from "react";
 import Product from './Product';
 
 const ProductList = props => {
-  const ProductItems = props.products.map((product, index) => (
+  const products = Object.keys(props.products).map(key => props.products[key]);
+  const ProductItems = products.map((product, index) => (
     <Product {...product} key={index} position={index} />
   ));
   return <ul>{ProductItems}</ul>;
--- src/products/index.js
+++ src/products/index.js
@@ -1,26 +1,26 @@
 import * as types from './types';
 import * as productActions from './actions';
 
-const initialState = [
-    {
+const initialState = {
+    'ff6fdc7a-3fa1-45da-803c-b1eeedf44a05': {
         imgSrc: "https://via.placeholder.com/100x100",
         name: "Javelin (caution)",
         price: 500.00,
         count: 0
     },
-    {
+    '404fba0b-2592-4999-bd20-d0bc03165214':{
         imgSrc: "https://via.placeholder.com/100x100",
         name: "Snail",
         price: 3.99,
         count: 0
     },
-    {
+   '29ebcb1c-5f23-4c51-9161-b773ed52fa1f': {
         imgSrc: "https://via.placeholder.com/100x100",
         name: "Rug",
         price: 93.47,
         count: 0
     }
-];
+};
 const reducer = (state=initialState, action) => {
     switch(action.type){
         case types.ADD_ITEM: { 
