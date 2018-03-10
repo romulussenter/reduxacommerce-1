75bb67cd98f95adcca5e65af829920d0364e9b76
Justin Dennison <justin1dennison@gmail.com>
2018-03-09 19:33:27 -0500

    connected the search bar with the store and mapped searchTerm to props as well as updateSearch to props.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 
 const SearchBar = props => {
     return (
-        <div>Search</div>
+        <div>
+            <input type="text"  
+                value={props.searchTerm}
+                onChange={(e) => props.updateSearch(e.target.value)}/>
+        </div>
     );
 }
 
-export default SearchBar;
+const mapStateToProps = state => {
+    return {
+        searchTerm: state.search
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        updateSearch: term => dispatch({type: 'UPDATE_SEARCH', payload: {value: term}})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
