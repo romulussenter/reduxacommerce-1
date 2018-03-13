46de94f99d94b8148ad0e6fbf768dfa881271d55
Justin Dennison <justin1dennison@gmail.com>
2018-03-12 20:41:26 -0400

    dynamically calculating the cart products count in the button

--- src/CartButton.js
+++ src/CartButton.js
@@ -5,14 +5,23 @@ import { cartActions } from './cart';
 
 
 const CartButton = props => {
+    const count = Object.keys(props.products)
+                        .map(key => props.products[key])
+                        .map(p => p.count)
+                        .reduce((prev, next) => prev + next, 0);
     return (
         <div>
+            <p>{count}</p>
             <button onClick={() => props.toggle() }>Cart</button>
         </div>
     );
 }
 
-const mapStateToProps = null;
+const mapStateToProps = state => {
+    return {
+        products: state.products
+    }
+};
 
 const mapDispatchToProps = dispatch => {
     return {
