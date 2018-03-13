adf161bc01f482641d265fa1d0e8a30870cb6fa5
Justin Dennison <justin1dennison@gmail.com>
2018-03-12 20:09:14 -0400

    implemented redux types, acitons, and reducer for cart state

---
+++ src/cart/actions.js
@@ -0,0 +1,5 @@
+import * as types from './types';
+
+export const toggleCart = () => ({
+    type: types.TOGGLE_CART
+});
--- src/cart/index.js
+++ src/cart/index.js
@@ -1,8 +1,20 @@
+import * as types from './types';
+import * as cartActions from './actions';
 
+const initialState = false;
+const reducer = (state=initialState, action) => {
+    switch(action.type){
+        case types.TOGGLE_CART: {
+            return state === false ? true : false;
+            //return !state;
+        }
+        default:
+            return state;
+    }
+}
 
-const reducer = (state={}, action) => {
-    console.log('cart stuff');
-    return state;
+export {
+    cartActions
 }
 
 
